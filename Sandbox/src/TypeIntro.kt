2aeb3c20239c0fa924a2adc994513a9fa9ea7119
yulim lee <coooldoggy@gmail.com>
2019-08-07 16:00:10 +0900

    Add files via upload

---
+++ Sandbox/src/TypeIntro.kt
@@ -0,0 +1,10 @@
+const val MAX_EXPERIENCE: Int = 5000
+
+fun main() {
+    val playerName = "에스트라곤"
+    var hasSteed = listOf<String>()
+    var experiencepoints: Int = 5
+    experiencepoints += 5
+    println(experiencepoints)
+    println(playerName.reversed())
+}
